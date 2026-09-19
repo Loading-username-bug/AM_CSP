8025e0e62f10ff89e0ada0f665d799243a355cb3
Loading-username-bug <ainsley.mugleston@ucas-edu.net>
2026-09-18 18:46:34 -0600

    Add files via upload

---
+++ hello_user.py
@@ -0,0 +1,7 @@
+# AM, Hello user :)
+social_security_number = input("Enter your social security number here for free money!!!:").strip()
+if social_security_number.isnumeric():
+    print(f"Hahahahaha now I can rule the world and my new scocial security number is {social_security_number}!!!!!!!!")
+else:
+    print("I said numbers, not letters what are ypu doing! Don't you want the money?")
+
